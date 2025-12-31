458b7d26c21a4cc3bfa869b1845c945a80182ba5
Dileep Kumar <chukkala.dileepkumar@gmail.com>
2025-12-31 09:42:37 +0530

    Refactor variable swap using array destructuring

--- 06-javascript/excercise-02/Variable-Swap.js
+++ 06-javascript/excercise-02/Variable-Swap.js
@@ -1,10 +1,12 @@
 let x = 5;
 let y = 10;
 
-x  = x + y;
-y = x - y;
-x = x - y;
+// Swap using array destructuring (modern & clean)
+[x, y] = [y, x];
 
-console.log('value of x = ',x); // Should be 10
+const logValues = () => {
+  console.log(`Value of x = ${x}`); // 10
+  console.log(`Value of y = ${y}`); // 5
+};
 
-console.log('value of y = ',y); // Should be 5
+logValues();
