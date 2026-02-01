255a9a8e5cd9ce722f1e795c3448e14db76f63ca
dileepkuamar <chukkala.dileepkumar@gmail.com>
2026-02-01 22:54:06 +0530

    aop and logging also added

--- 15-spring-boot/revpay-p2/src/main/java/com/example/revpay_p2/aspect/LoggingAspect.java
+++ 15-spring-boot/revpay-p2/src/main/java/com/example/revpay_p2/aspect/LoggingAspect.java
@@ -1,24 +1,43 @@
 package com.example.revpay_p2.aspect;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    // Runs before any method in service package
+    private static final Logger log = LogManager.getLogger(LoggingAspect.class);
+
+    // Before service methods
     @Before("execution(* com.example.revpay_p2.service.*.*(..))")
     public void beforeServiceMethods(JoinPoint joinPoint) {
-        System.out.println("👉 Entering method: " + joinPoint.getSignature());
+        log.info("👉 Entering method: {}", joinPoint.getSignature());
     }
 
-    // Runs after any method in service package
+    // After service methods
     @After("execution(* com.example.revpay_p2.service.*.*(..))")
     public void afterServiceMethods(JoinPoint joinPoint) {
-        System.out.println("✅ Exiting method: " + joinPoint.getSignature());
+        log.info("✅ Exiting method: {}", joinPoint.getSignature());
+    }
+
+    // When any exception occurs in service layer
+    @AfterThrowing(
+            pointcut = "execution(* com.example.revpay_p2.service.*.*(..))",
+            throwing = "ex"
+    )
+    public void logExceptions(JoinPoint joinPoint, Exception ex) {
+
+        log.error("❌ Exception in method: {} | Message: {}",
+                joinPoint.getSignature(),
+                ex.getMessage(),
+                ex
+        );
     }
 }
